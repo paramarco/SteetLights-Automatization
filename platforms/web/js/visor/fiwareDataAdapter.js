7075dc4c7fa7df01976f63187dfbe57ad0aa4662
fiwareRockStar <julky@iTodonte.local>
2014-04-24 22:49:08 +0200

    visor datos

---
+++ platforms/web/js/visor/fiwareDataAdapter.js
@@ -0,0 +1,114 @@
+var fiwareDataAdapter = (function () {
+ 
+        var ip = "217.127.199.47:8080";
+       
+       function setIP(newIP){
+            ip = newIP;
+       }
+
+       function processLampsData(data){
+             
+              var deferred  = $.Deferred();
+              var results    = [];
+              var lamps     = data.contextResponses || [];
+
+              //process data
+                for(var i=0,n=lamps.length; i<n; i++) {
+                    
+                    var lampData  = lamps[i].contextElement;
+                    var cabinetID  = parseInt(lampData.attributes[2].value);
+                    var lampID      = parseInt(lampData.id);
+
+                    if (!(cabinetID in results)) {
+                        results[cabinetID] = {};
+                    }
+
+                    results[cabinetID][lampID] = {
+                        id : lampID,
+                        luminosityLevel : lampData.attributes[0].value,
+                        position : lampData.attributes[1].value,
+                        electricalCabinetID : cabinetID
+                    };
+                }
+
+              deferred.resolve(results);
+              return deferred.promise();
+        }
+       
+        function processSensorsData(data){
+              var deferred  = $.Deferred();
+              var results     = [];
+              var sensors    = data.contextResponses || [];
+
+              //process data
+                for(var i=0,n=sensors.length; i<n; i++) {
+                    
+                    var sensorData  = sensors[i].contextElement;
+                    var sensorType  = sensorData.attributes[0].name;
+                    var sensorID      = parseInt(sensorData.id);
+                   
+                    if (!(sensorType in results)) {
+                        results[sensorType] = {};
+                    }
+
+                    results[sensorType][sensorID] = {
+                        id : sensorID,
+                        type : sensorData.attributes[0].name,
+                        unit : sensorData.attributes[0].type,
+                        value : sensorData.attributes[0].value,
+                        position : sensorData.attributes[2].value
+                    };
+                }
+
+              deferred.resolve(results);
+              return deferred.promise();
+        }
+
+        function loadLamps() {
+                return $.ajax({
+                   url: 'http://'+ip+'/NGSI10/contextEntityTypes/luminaria',
+                   type: 'GET',
+                   beforeSend: function(xhr) {
+                       xhr.setRequestHeader("Content-type","application/json; charset=utf-8");
+                       xhr.setRequestHeader("Accept","application/json;");
+                   }}).then(processLampsData); 
+        }
+ 
+        function loadSensors() {
+          return $.ajax({
+                     url: 'http://'+ip+'/NGSI10/contextEntityTypes/sensor',
+                     type: 'GET',
+                     beforeSend: function(xhr) {
+                         xhr.setRequestHeader("Content-type","application/json; charset=utf-8");
+                         xhr.setRequestHeader("Accept","application/json;");
+                     }}).then(processSensorsData); 
+        }
+ 
+        function _updateLuminosityLamp(id,luminosityLevel) {
+
+                var data  = {
+                    "contextElements":[
+                          {"type":"luminaria","id":id,"attributes":[{"name":"nivelIntensidad","type":"porcentaje","value": luminosityLevel}]}
+                    ],
+                    "updateAction":"UPDATE"
+                };
+
+                return  $.ajax({
+                   url: 'http://'+ip+'/NGSI10/updateContext',
+                   type: 'POST',
+                   data: JSON.stringify(data),
+                   beforeSend: function(xhr) {
+                       xhr.setRequestHeader("Content-type","application/json; charset=utf-8");
+                       xhr.setRequestHeader("Accept","application/json;");
+                   }});
+                   // .then(processResult);  process result !! 
+        }
+  
+        return {
+            setIP : setIP,
+            loadLamps: loadLamps,
+            loadSensors: loadSensors,
+            updateLuminosityLamp: _updateLuminosityLamp
+        };
+ 
+    })();
---
+++ platforms/web/js/visor/infoWindowTemplates.js
@@ -0,0 +1,50 @@
+var infoWindowLampTemplate = (function () {
+     
+        function fillTemplate(data){
+                return "<div id='lamp-"+data.id+"' class='bubble'>\
+                                <h1 class='title'>Id: "+data.id+"</h1>\
+                                <div class='info'>\
+                                    <p><span>Coordenadas: </span><span class='nTotal'>"+data.position+"</span></p>\
+                                    <p><span>Cuadro asociado: </span><span>"+data.electricalCabinetID+"</span></p>\
+                                    <p class='luminosityLevel'><span>Nivel de iluminación (%): </span></p>\
+                                    <form oninput='amount.value=rangeInput.value'>\
+                                            <input id ='range-"+data.id+"' class='luminosityRange' type='range' name='rangeInput' min='0' max='100' step='10' value='"+data.luminosityLevel+"'>\
+                                            <output class='luminosityValue' name='amount' for='rangeInput'>"+data.luminosityLevel+"</output><span></span>\
+                                    </form>\
+                                </div>\
+                            <div class='clear'></div>\
+                        </div>";
+        }
+
+        function addListenerToLuminosityRange(id,callback){
+            document.getElementById("range-"+id).onchange = callback;
+        }
+
+        return {
+            fillTemplate: fillTemplate,
+            addListenerToLuminosityRange: addListenerToLuminosityRange
+        };
+ 
+    })();
+
+    var infoWindowSensorTemplate = (function () {
+     
+        function fillTemplate(data){
+                return "<div id='lamp-"+data.id+"' class='bubble'>\
+                                <h1 class='title'>Id: "+data.id+"</h1>\
+                                <div class='info'>\
+                                    <p><span>Sensor de tipo: </span><span>"+data.type+"</span></p>\
+                                    <p><span>Coordenadas: </span><span class='nTotal'>"+data.position+"</span></p>\
+                                    <p><span>Unidad de medida: </span><span>"+data.unit+"</span></p>\
+                                    <p><span>Valor: </span><span>"+data.value+"</span></p>\
+                            </div>\
+                            <div class='clear'></div>\
+                        </div>";
+        }
+
+
+        return {
+            fillTemplate: fillTemplate
+        };
+ 
+    })();
