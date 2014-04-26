9bd7ce7c647081f7e883e8dc1b03c80bb57e9785
fiwareRockStar <julky@iTodonte.local>
2014-04-26 19:14:48 +0200

    proxy server

---
+++ platforms/proxy/proxy.js
@@ -0,0 +1,50 @@
+//CORS Server
+var express    = require('express'),
+    cors          = require('cors'),
+    corsApp    = express(),
+    corsServer = require('http').createServer(corsApp);
+
+var log4js = require('log4js');
+var logger = log4js.getLogger();
+
+var contextBroker = require('dev-rest-proxy');
+
+corsApp.disable('x-powered-by');
+corsApp.use(cors());
+corsApp.use(corsApp.router);
+
+corsApp.all('/NGSI10/*', function(req, res, next){
+    contextBroker.proxy(req, res, '127.0.0.1', 1026);
+});
+
+corsServer.listen(8080, function(){
+  logger.info('CORS proxy on port 8080');
+});
+
+
+
+
+//websocket Server notify
+var webSocketApp     = express(),
+      webSocketServer = require('http').createServer(webSocketApp),
+      io                       = require('socket.io').listen(webSocketServer, {log:false});
+
+webSocketApp.use(express.json({strict:false}));
+webSocketApp.disable('x-powered-by');
+
+webSocketApp.post('/notify', function (req, res) {
+     res.send('');
+     logger.trace(JSON.stringify(req.body));
+     io.sockets.emit('update',req.body);
+});
+
+io.sockets.on('connection', function (socket) {
+//  socket.emit('news', { hello: 'world' });
+//  socket.on('myEvent', function (data) {
+//         console.log(data);
+//  });
+});
+
+webSocketServer.listen(8090, function(){
+  logger.info('Socket server on port 8090');
+});
