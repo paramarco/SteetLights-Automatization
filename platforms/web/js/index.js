a9de8ed06873bfc5b243631e6620250c6a46e36e
Marco Vereda Manchego <paramarco@gmail.com>
2014-05-02 13:47:54 +0200

    seleccion de IPs por servidor
    Signed-off-by: Marco Vereda Manchego <paramarco@gmail.com>

--- platforms/web/js/index.js
+++ platforms/web/js/index.js
@@ -2,8 +2,7 @@ $( document ).ready(function() {
 
 		$("#selectServidorFIware").change(function(){	
             app.plataformaObjetivo = "fiware";
-            asignarServidorObjetivo();
-            alert("servidor cambiado");	
+            asignarServidorObjetivo();            
         });
             
         $("#boton_genera_objetos").click(function(){	
@@ -62,6 +61,11 @@ $( document ).ready(function() {
             } else {
                     alert("Inserte un numero por favor!");
             }
+        });
+        
+        $("#selectServidorSOFIA").change(function(){	
+            app.plataformaObjetivo = "sofia";
+            asignarServidorObjetivo();
         });													
 
         $("#boton_genera_objetos_sofia").click(function(){	
@@ -89,6 +93,7 @@ $( document ).ready(function() {
 
         $("#botonIrMonitorFiware").click(function(){	
             app.plataformaObjetivo = "fiware";
+            asignarServidorObjetivo();
             router_to_widget();
             visor.run(fiwareDataAdapter,fiwareNotifier);
          
@@ -96,7 +101,8 @@ $( document ).ready(function() {
 
         $("#botonIrMonitorSofia").click(function(){	
             app.plataformaObjetivo = "sofia";
-            router_to_widget();
+            asignarServidorObjetivo();
+            router_to_widget();            
             visor.run(sofia2DataAdapter,sofia2Notifier);
         });
 
@@ -111,17 +117,83 @@ function router_to_widget(){
 
 function asignarServidorObjetivo(){
 	if (app.plataformaObjetivo == "sofia")	{
-		console.log( "DEBUG : 	Entra en 	: lanzaSimulacion: con SOFIA "   );
-		var numeroServidor = parseInt($("#selectServidorFIware").val());
-		///switch
+		var numeroServidor = parseInt($("#selectServidorSOFIA").val());
+		switch(numeroServidor)
+		{
+		case 1:
+		 pathToDwrServlet = app.ipSOFIAinCloud;
+		 sibServer = pathToDwrServlet + '/';
+		 
+		 app.luminaria.ontologia = "SIB_test_luminaria";
+		 app.luminaria.KP = "KP_test_luminaria";
+		 app.luminaria.instancia = "KP_test_luminaria:KP_test_luminaria01";
+		 app.luminaria.token = "3bb7264f5c1743b78dbaa5ba2e33ac35";
+		 
+		 app.cuadro.ontologia = "SIB_test_cuadro";
+		 app.cuadro.KP = "KP_test_cuadro";
+		 app.cuadro.instancia = "KP_test_cuadro:KP_test_cuadro01";
+		 app.cuadro.token = "6cb9fa1dcd404093ac38997eb1f3d620";
+		 
+		 app.sensor.ontologia = "SIB_test_sensor";
+		 app.sensor.KP = "KP_test_sensor";
+		 app.sensor.instancia = "KP_test_sensor:KP_test_Sensor02";
+		 app.sensor.token = "80fb6498a34e48caa6a1f68ca91dda7a";
+		 
+		  break;
+		case 2:
+		 pathToDwrServlet = app.ipSOFIAInstalTIC; 
+		 sibServer = pathToDwrServlet + '/';
+		 
+		 app.luminaria.ontologia = "SIB_test_luminaria";
+		 app.luminaria.KP = "KP_test_luminaria";
+		 app.luminaria.instancia = "KP_test_luminaria:KP_test_luminaria01";
+		 app.luminaria.token = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+		 
+		 app.cuadro.ontologia = "SIB_test_cuadro";
+		 app.cuadro.KP = "KP_test_cuadro";
+		 app.cuadro.instancia = "KP_test_cuadro:KP_test_cuadro01";
+		 app.cuadro.token = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+		 
+		 app.sensor.ontologia = "SIB_test_sensor";
+		 app.sensor.KP = "KP_test_sensor";
+		 app.sensor.instancia = "KP_test_sensor:KP_test_Sensor02";
+		 app.sensor.token = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+		  break;
+		default:
+		}
 		
-		//setKpName("KP_test_luminaria");	
-		//conectarSIBConToken("3bb7264f5c1743b78dbaa5ba2e33ac35", "KP_test_luminaria:KP_test_luminaria01", arrancaSimulacion );
-
+		//fiwareNotifier.setLuminariaSettings(app.luminaria);
+		//fiwareDataAdapter.setLuminariaSettings(app.luminaria);
+		
+		//fiwareNotifier.setCuadroSettings(app.cuadro);
+		//fiwareDataAdapter.setCuadroSettings(app.cuadro);
+		
+		//fiwareNotifier.setLuminariaSettings(app.sensor);
+		//fiwareDataAdapter.setLuminariaSettings(app.sensor);
+		
+		console.log( "DEBUG : RECONFIGURACION DE IPS SOFIA: servidor: " + sibServer +" " +  JSON.stringify(app.luminaria) + JSON.stringify(app.cuadro) + JSON.stringify(app.sensor) );
+			
 	}
 	else //fiware
 	{
-	}
+		var numeroServidor = parseInt($("#selectServidorFIware").val());
+		switch(numeroServidor)
+		{
+		case 1:
+		 app.ipFIware = app.ipFIwareInstalTIC;
+		 app.ipFIwareNotifier = app.ipFIwareNotifierInstalTIC;
+		  break;
+		case 2:		 
+		 app.ipFIware = app.ipFIwareFIlab;
+		 app.ipFIwareNotifier = app.ipFIwareNotifierFIlab;
+		  break;
+		default:
+		}
+		fiwareNotifier.setIP(app.ipFIware);
+		fiwareDataAdapter.setIP(app.ipFIwareNotifier);
+			
+		console.log( "DEBUG : RECONFIGURACION DE IPS FIWARE :" +  app.ipFIware + " notificador " + ipFIwareNotifier    );
+	}	
 }
 
 
@@ -129,20 +201,27 @@ var app = {
     // app atributtes    
     plataformaObjetivo: undefined,
     manejadorCiclo: undefined,
-    
+
+    //IPs de arranque
     ipFIware : "217.127.199.47:8080",
     ipFIwareNotifier :  "217.127.199.47:8090",
+    
+    ipSOFIA : 'http://scfront.cloudapp.net/sib/dwr',   
+    
+    //IP segun modo cloud o en InstalTIC
     
     ipFIwareInstalTIC : "217.127.199.47:8080",
     ipFIwareNotifierInstalTIC :  "217.127.199.47:8090",
     
-    ipFIwareFIlab : "217.127.199.47:8080",
-    ipFIwareNotifierFIlab:  "217.127.199.47:8090",
+    ipFIwareFIlab : "130.206.83.60:8080",
+    ipFIwareNotifierFIlab:  "130.206.83.60:8090",
     
-    ipSOFIA : "",
+    ipSOFIAinCloud : 'http://scfront.cloudapp.net/sib/dwr',
+    ipSOFIAInstalTIC : 'http://217.127.199.47:9090/sib/dwr',
     
-    ipSOFIAinCloud : "",
-    ipSOFIAInstalTIC : ""
-    
+    //Ontologias,KP,Tokens e instancias
+    luminaria : { ontologia : "SIB_test_luminaria", KP : "KP_test_luminaria", instancia : "KP_test_luminaria:KP_test_luminaria01", token :"3bb7264f5c1743b78dbaa5ba2e33ac35" },
+    sensor : { ontologia : "SIB_test_sensor", KP : "KP_test_sensor", instancia : "KP_test_sensor:KP_test_Sensor02", token :"80fb6498a34e48caa6a1f68ca91dda7a" },
+    cuadro : { ontologia : "SIB_test_cuadro", KP : "KP_test_cuadro", instancia : "KP_test_cuadro:KP_test_cuadro01", token :"6cb9fa1dcd404093ac38997eb1f3d620" }    
     
 };
